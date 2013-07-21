b235b7a22620c6ebc38630695fb020539c320f61
Hu Chen <huch0003@gmail.com>
2013-07-21 13:09:48 +0800

    initial commit

---
+++ app/assets/javascripts/backbonetunerails.js.coffee
@@ -0,0 +1,9 @@
+window.Backbonetunerails =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  Backbonetunerails.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backbonetunerails::Application.config.secret_token = '3cabd15097e55abc6d377eb3ead7b02d99373b6cccb12f18c3c078b0aff8291cd2dc3f92f9810cec4df7456e52cd388b7d8e15c799e3098855770df799357133'
